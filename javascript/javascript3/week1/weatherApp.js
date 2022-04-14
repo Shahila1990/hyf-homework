af05d54d0986d03f690b075d0af731b68004cc3a
Shahila Mafraz <fathima.shahila371@gmail.com>
2022-04-14 12:59:05 +0800

    modified code

--- javascript/javascript3/week1/weatherApp.js
+++ javascript/javascript3/week1/weatherApp.js
@@ -22,17 +22,16 @@ form.addEventListener("submit", (e) => {
     alert("Please type a city name");
   } else {
     city = search.value;
-    fetchWeatherData();
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=d09e6b8bf67128a701c0f9da9000852d`;
+    fetchWeatherData(url);
     search.value = "";
     app.style.opacity = ".5";
   }
   e.preventDefault();
 });
 
-function fetchWeatherData() {
-  fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=d09e6b8bf67128a701c0f9da9000852d`
-  )
+function fetchWeatherData(url) {
+  fetch(url)
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
@@ -46,7 +45,7 @@ function fetchWeatherData() {
       //date
       const dt = data.dt;
       dateOutPut.innerHTML = new Date(dt * 1000).toDateString();
-      
+
       //time
       const timezone = data.timezone;
       const timezoneInMinutes = timezone / 60;
@@ -67,11 +66,52 @@ function fetchWeatherData() {
       icon.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
 
       //map
-      
-// function initMap(){
-//   map = new google.maps.Map(document.getElementById('map'), {
-//     center: { lat: -34.397, lng: 150.644 },
-//     zoom: 8,
-//   });
-    });
+
+      /* function initMap() {
+        let country = {lat: data.coord.lat, lng: data.coord.lon}
+        let map = new google.maps.Map(
+          document.getElementById('map'), {zoom:4,center:country}
+        )
+        let marker = new google.maps.Marker({position: country, map:map})
+      }
+      initMap() */
+    })
+    .catch((err) => alert("country not found"));
 }
+
+/* Get current location */
+const locationBtn = document.getElementById("locationbtn");
+locationBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  window.navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
+  function geoSuccess(position) {
+    const lat = position.coords.latitude;
+    const lng = position.coords.longitude;
+    console.log("lat:" + lat + " lng:" + lng);
+    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=276989726f58d1c881ca8aff464bf98a`;
+    fetchWeatherData(url);
+  }
+  function geoError() {
+    alert("failed to get location.");
+  }
+});
+
+/* Store location in local Storage */
+
+btn.addEventListener("click", (e) => {
+  //e.preventDefault();
+  var locationArr = [];
+  var location_name = search.value;
+  locationArr.push(location_name);
+  localStorage.setItem("ls_location_name", JSON.stringify(locationArr));
+});
+const prevSearch = document.getElementById("prevSearch");
+prevSearch.innerHTML = JSON.parse(localStorage.getItem("ls_location_name"));
+
+//previous search
+prevSearch.addEventListener("click", (e) => {
+  city = e.target.innerHTML;
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=d09e6b8bf67128a701c0f9da9000852d`;
+  fetchWeatherData(url);
+  app.style.opacity = "1";
+});
